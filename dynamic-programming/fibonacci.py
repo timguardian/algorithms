f11ef188bbfadd6b8ac73488dc42fb549da2efa8
timguardian <duisengaliyev.temirlan@gmail.com>
2020-11-14 17:10:27 +0600

    Applyed memoization technique and got huge decrease of computation time

--- dynamic-programming/fibonacci.py
+++ dynamic-programming/fibonacci.py
@@ -1,13 +1,24 @@
 import timeit
 
 
-def fib(n):
+def fib(n, cache=None):
     if n == 0:
         return 1
     if n == 1:
         return 1
-    return fib(n-1) + fib(n-2)
+
+    if cache is None:
+        cache = {}
+
+    if n in cache:
+        return cache[n]
+
+    result = fib(n - 1, cache) + fib(n - 2, cache)
+
+    cache[n] = result
+
+    return result
 
 
 if __name__ == "__main__":
-    print(timeit.timeit('fib(25)', number=100, globals=globals()))
+    print(timeit.timeit('fib(100)', number=100, globals=globals()))
