ca8bbe524af9b96a13f660e5172353fdfd3d3d42
timguardian <duisengaliyev.temirlan@gmail.com>
2020-11-14 17:25:57 +0600

    Dynamic programming. Applyed bottom-up approach, gain huge amount of computation time decrease and way much less memory usage

--- dynamic-programming/fibonacci.py
+++ dynamic-programming/fibonacci.py
@@ -2,22 +2,12 @@ import timeit
 
 
 def fib(n, cache=None):
-    if n == 0:
-        return 1
-    if n == 1:
-        return 1
+    a = 1  # fib(i - 2)
+    b = 1  # fib(i - 1)
 
-    if cache is None:
-        cache = {}
-
-    if n in cache:
-        return cache[n]
-
-    result = fib(n - 1, cache) + fib(n - 2, cache)
-
-    cache[n] = result
-
-    return result
+    for i in range(2, n + 1):
+        a, b = b, a + b
+    return b
 
 
 if __name__ == "__main__":
